e0313ab60b687b3a1afde6f501e562fe688a160e
Fire <firetryant@gmail.com>
2019-05-26 22:06:32 +0800

    work

--- src/com/neuedu/fire/text_class.java
+++ src/com/neuedu/fire/text_class.java
@@ -1,8 +1,17 @@
 package com.neuedu.fire;
 
 public class text_class {
-    public   static void main(String[]args){System.out.println("hello word " +
-            "FIRE IS COMMING");
+    public static void main(String[] args) {
+        System.out.println("hello word " + "FIRE IS COMMING");
+        int a = 4;
+        if (a >= 42) {
+            System.out.println("a为良品");
+        } else if (a < 42) {
+            System.out.println("a为残次品");
+        }
+        else{ System.out.println("a为优品");}
+
 
     }
 }
+
