3712994c88e5570ac22755c0d5dd136d6aa47be3
Fire <firetryant@gmail.com>
2019-05-29 23:23:50 +0800

    work

---
+++ src/com/neuedu/fire/circle.java
@@ -0,0 +1,14 @@
+package com.neuedu.fire;
+
+public class circle {
+    int redues;
+    public void  setRedues(int redues1){
+    if (redues1<0){System.out.println("半径不能为负数，你这是在为难我胖虎！！！");}
+    else {redues=redues1;}}
+    public  int  getRedues(){return redues;}
+    public  double zc (){ return Math.PI*redues*2;}
+
+    public  double mianji(){  return  Math.PI*redues*redues;}
+// public  double arer(){ return  Math.PI*Math.pow(redues,2);}
+
+}
